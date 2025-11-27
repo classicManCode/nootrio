c68c21708b0ea898f4437d19c523f5c285c79084
ClassicMan <legenderyprime@gmail.com>
2025-11-27 20:07:57 +0100

    Navbar done for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <section className="h-screen w-full">
+      <Image
+        src="/hero-image.png"
+        alt="Hero Image"
+        width={2000}
+        height={1333}
+        style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        priority
+      />
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+          backgroundColor: "black",
+          opacity: 0.4, // 👈 adjust this (0.1–0.7)
+        }}
+      ></div>
+    </section>
+  );
+};
 
-export default Page
+export default Page;
