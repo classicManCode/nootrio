aa2d86ed9bffd3843a0f09d8c0cefc51964cf991
ClassicMan <legenderyprime@gmail.com>
2025-11-28 12:49:35 +0100

    Hero section fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 import Image from "next/image";
+import Navbar from "@/component/Navbar";
+import Hero from "@/component/Hero";
+
 
 const Page = () => {
   return (
-    <section className="h-screen w-full">
-      <Image
-        src="/hero-image.png"
-        alt="Hero Image"
-        width={2000}
-        height={1333}
-        style={{ objectFit: "cover", width: "100%", height: "100%" }}
-        priority
-      />
-      <div
-        style={{
-          position: "absolute",
-          inset: 0,
-          backgroundColor: "black",
-          opacity: 0.4, // 👈 adjust this (0.1–0.7)
-        }}
-      ></div>
-    </section>
+    <main className="relative h-screen w-full">
+      <Navbar />
+      <Hero />
+    </main>
   );
 };
 
---
+++ component/Navbar.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { FaThreads } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    // if not "fixed" then add "absolute"
+    <div className="flex justify-center items-center fixed w-full top-4 z-50"> 
+      <nav className="flex items-center justify-between w-[65%]">
+        <Link href="/">LOGO</Link>
+        <ul className="flex gap-8 items-center py-5 rounded-full px-10 bg-teal-800/40 backdrop-blur-[10px]">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/services">Services</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="flex gap-4">
+          <a
+            href="https://www.threads.com/@jiggybenn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaThreads />
+          </a>
+          <a
+            href="https://x.com/Classicmancode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            href="https://github.com/classicManCode"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
