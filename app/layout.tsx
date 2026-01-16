c0b9cc42b4b1a10c487d0c45992459ea1ac0e119
Benjamin <legenderyprime@gmail.com>
2026-01-16 20:28:02 +0100

    Impoving SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/component/Footer";
-import Navbar from "@/component/Navbar";
-import SmoothScroll from "@/component/SmoothScroll";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import SmoothScroll from "@/components/SmoothScroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,13 +18,16 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: {
     default: "Nootrio Health Blog",
-    template: "%s - Nootrio"
+    template: "%s - Nootrio",
   },
   description:
     "Nootrio is a food-first blog that explores the pros and cons of common foods so you can make smarter choices about what you eat.",
   twitter: {
     card: "summary_large_image",
-  }
+  },
+  verification: {
+    google: "br85fb2aKlNnWn1tJMsug--YFxy3wdQjvwM3AK9HT8o",
+  },
 };
 
 export default function RootLayout({
